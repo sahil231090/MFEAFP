776d79567f9d0724e791d66afc00dd0c3f1539e7
sahil231090 <sahil.231090@gmail.com>
2017-03-28 14:04:50 -0700

    Add files via upload

---
+++ testing_file.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 20:03:48 2017
+
+@author: sahil.231090
+"""
+
+from utils import get_headlines, headline_to_svo, get_comp_df, map_ticker
+
+headline_df = get_headlines()
+comp_df = get_comp_df()
+
+num_headlines, headline_file_cols = headline_df.shape
+text = headline_df.loc[1, 'headline']
+headline_date = headline_df.loc[1, 'date']
+subject, verb, obj =  headline_to_svo(text)
+ticker = map_ticker(text, headline_date, comp_df)
+
+
+# Tests
+assert num_headlines == 1869136
+assert headline_file_cols == 2
+assert text == 'Corning (GLW) Crosses Pivot Point Support at $23.01'
+assert subject == 'Corning'
+assert verb == 'crosses'
+assert obj == 'Pivot Point Support'
